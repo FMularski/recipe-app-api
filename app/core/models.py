872484a768b8e65c6f127144208b0b128a7fdde5
Filip <filipelblag@gmail.com>
2023-05-30 16:15:45 +0200

    implemented and tested User model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email address.")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        return self.create_user(email, password, is_superuser=True, is_staff=True, **extra_fields)
+
+
+class User(PermissionsMixin, AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.name
