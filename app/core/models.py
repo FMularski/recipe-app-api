76eced9b51dfb73ca2e3959bf2872a4948475425
Filip <filipelblag@gmail.com>
2023-05-31 16:47:55 +0200

    created and tested Recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
@@ -29,3 +30,17 @@ class User(PermissionsMixin, AbstractBaseUser):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="recipes"
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+from core import models
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -32,3 +35,16 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user("test@example.com", "sample123")
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Test recipe",
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Test desc",
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
